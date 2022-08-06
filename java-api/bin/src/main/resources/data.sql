98370e307a537464373b1994855129bd103d967a
Siya6688 <siyaswami6688@gmail.com>
2022-08-06 23:51:45 +0530

    exceptions in schema.sql resolved

---
+++ java-api/bin/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO dogs (id, name, age) VALUES (1, 'Einstein', 3);
+INSERT INTO dogs (id, name, age) VALUES (2, 'Kaya', 5);
+INSERT INTO users (id, name, email, role) VALUES (1, 'salvin', 'salvinkajar000@gmail.com','analyst');
+
+INSERT INTO securities  VALUES (1, 100,100,'DB','2022-08-05',5,'bond',15,'active');
+
